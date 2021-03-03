1b4c09600c56166ffaf23e26de5d7d05d152b4b7
Xuhua Hu <HUXUHUA@kingsoft.com>
2021-03-03 22:39:35 +0800

    update for my lover

--- how_to_process_tags.py
+++ how_to_process_tags.py
@@ -1,14 +1,14 @@
-
 # coding: utf-8
 
 # In[1]:
 
+WORDS = "resort summer monroe marilyn scarlet wine maroon crimson gauze vacation chiffon bohemian nymph fairy mesh pixie whimsical boho bohemian hippie through hypster silk sax victorian drapey  monroe sheer ethereal veil crepe Alexander vampire Mcqueen Jean Chloe paul gaultier"
 
-WORDS= "mongolian lamb wool mohair sheep tibetan penny lane Sloppy rock punk wild west gypsy hipster ghetto artisanal artist Y2K gritty folk bohemian grunge boho hippie alternative upcycled remade artsy club kids mod moschino Marques Almeida raw"
+
 SIZE_OF_CHARACTERS_IN_A_ROW = 20
 
 size_of_chars = SIZE_OF_CHARACTERS_IN_A_ROW
-words= WORDS.split()
+words = list(set(WORDS.split()))
 
 def helper(_start, _words, _sums):
     i = _start
@@ -31,12 +31,14 @@ def helper(_start, _words, _sums):
 
     return sums_
 
+i = 0
+
 while len(words) > 0:
     sums = {}
     sums = helper(0, words, sums)
     best_item = sorted(sums.items(), key=lambda item:item[1], reverse=True)[0]
-    print(best_item)
-    
+    i += 1
+    print(f"{i}: {best_item}")
     removed_words = best_item[0].split()
     for word in removed_words:
         words.remove(word)
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Modify the WORDS in how_to_process_tags.py, and run following instruction: 
+
+```
+python how_to_process_tags.py
+```
